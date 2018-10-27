bcb9c1433ef5d648c490976ba323f3b938df150a
kim-taewoo <acoustic0419@gmail.com>
2018-10-28 02:01:36 +0900

    added an article

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
       { text: 'Home', link: '/' },
       { text: 'Vue', link: '/vue/' },
       { text: 'Firebase', link: '/firebase/'},
-      { text: '네이버카페', link: 'https://cafe.naver.com/skkulikelion' },
+      { text: '멋사 네이버카페', link: 'https://cafe.naver.com/skkulikelion' },
       { text: 'Vue 공식문서', link: 'https://vuejs.org/v2/guide/'}      
     ],
     // sidebar: [
